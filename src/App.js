8b928c67df3182cddcaafa38b8df56f3743ee546
fanelob46 <fanelob46@gmail.com>
2024-07-11 15:21:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import './Home'
+import HomePage from './Home';
+
+function App() {
+  return (
+   <HomePage></HomePage>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,27 @@
+function HomePage()
+{
+  return <div className="section1">
+    <div className="nav">
+        <nav>
+            <h2 className="logo">D<span>Z</span></h2>
+            <ul>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Articles</a>
+                </li>
+            </ul>
+            <button type="button">Subcribe</button>
+        </nav>
+    </div>
+    <div className="home-info">
+        <div className="home-text">
+     <h1>DRONOTICZ</h1>
+    <p className="eraofdrones">THE ERA OF DRONES</p>
+        </div>
+       
+    </div>
+  </div>;
+}
+export default HomePage;
